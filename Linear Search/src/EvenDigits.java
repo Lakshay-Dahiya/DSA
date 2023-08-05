c87f042ab7b8ccce7349a8ab419fd305f548457b
Lakshay-Dahiya <lakshaydahiya2003@gmail.com>
2023-08-05 22:36:31 +0530

    Linear search

---
+++ Linear Search/src/EvenDigits.java
@@ -0,0 +1,44 @@
+public class EvenDigits {
+    public static void main(String[] args) {
+        int[] nums = {12,345,2,6,7896};
+        System.out.println(findNumbers(nums));
+
+    }
+    static int findNumbers(int[] nums){
+        int count=0;
+        for(int num:nums){
+            if (even(num)) {
+                count++;
+            }
+        }return count;
+    }
+    static boolean even(int num){
+        int numberofDigits = digits(num);
+        if(numberofDigits % 2 ==0){
+            return true;
+        }return false;//
+    }
+    static int digits(int num){
+        if(num < 0)
+        {
+            num = num * -1; // for negative numbers
+        }
+        if (num==0){
+            return 1; //if num =0, it has 1 digit
+        }
+        int count = 0;
+        while(num>0){
+            count++;
+            num/=10; //num=num/10;
+
+        }return count;//this is slower than digits2 because while loop runs for every number
+
+    }
+    static int digits2(int num){
+        if(num < 0)
+        {
+            num = num * -1; // for negative numbers
+        }
+        return (int)(Math.log10(num)) + 1;//shortcut to find number of digits
+    }
+}
