130522f6090a51e15bc583b821fcd2c2d3d6abde
Lakshay-Dahiya <lakshaydahiya2003@gmail.com>
2023-08-05 23:29:10 +0530

    Update MaxWealth.java

--- Linear Search/src/MaxWealth.java
+++ Linear Search/src/MaxWealth.java
@@ -7,7 +7,7 @@ public class MaxWealth {
     public int maximumWealth(int[][accounts]){
         //person=row , account=column
         int ans = Integer.MIN_VALUE;
-        for (int person = 0; person < acoounts.length; person++) {
+        for (int person = 0; person < accounts.length; person++) {
             int sum = 0;
             for (int account = 0; account < accounts[person].length; account++) {
                 sum= sum + accounts[person][account];
