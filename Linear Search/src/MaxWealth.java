f9a721e8daaddfd99a08ed5c89cd1d187e358832
Lakshay-Dahiya <lakshaydahiya2003@gmail.com>
2023-08-05 23:15:56 +0530

    Update MaxWealth.java

--- Linear Search/src/MaxWealth.java
+++ Linear Search/src/MaxWealth.java
@@ -1,5 +1,15 @@
 public class MaxWealth {
     public static void main(String[] args) {
 
+
+    }
+    public int maximumWealth(int[][ accounts]){
+        //person=row , account=column
+        for (int person = 0; person < acoounts.length; person++) {
+            for (int account = 0; account < accounts[person].length; account++) {
+
+            }
+        }
+
     }
 }
