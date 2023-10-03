2e12c3fac3d07e4c7099ac04bcfc7fe8d4fed837
Lakshay-Dahiya <lakshaydahiya2003@gmail.com>
2023-10-03 23:49:47 +0530

    Create Fibonacci.java

---
+++ Recursion Level - 1/src/Fibonacci.java
@@ -0,0 +1,15 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        // fib(5);
+        System.out.println(fib(5));
+        // int finalans= fib(5)
+        // sout(finalans);
+    }
+    public static int fib(int n){
+        if(n < 2){
+            return n;
+        }
+        int ans = fib(n-2) + fib(n-1);
+        return ans;
+    }
+}
